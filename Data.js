68d251e1633eb06e8eed91f411476120bfeaae91
mypastorjoe4u <143444008+mypastorjoe4u@users.noreply.github.com>
2024-03-03 07:22:45 +0100

    Add files via upload
    part ofmy learning process i created a crypto currency page. plrease share

---
+++ Data.js
@@ -0,0 +1,53 @@
+import Company1 from "./src/assets/1.png"
+import Company2 from "./src/assets/2.png"
+import Company3 from "./src/assets/3.png"
+import Company4 from "./src/assets/4.png"
+import Company5 from "./src/assets/5.png"
+
+import Icon1 from "./src/assets/icon-1.png";
+import Icon2 from "./src/assets/icon-2.png";
+import Icon3 from "./src/assets/icon-3.png";
+
+
+
+
+export const companyInfo = [
+  {
+    id: 1,
+    img: Company1,
+  },
+  {
+    id: 2,
+    img: Company2,
+  },
+  {
+    id: 3,
+    img: Company3,
+  },
+  {
+    id: 4,
+    img: Company4,
+  },
+  {
+    id: 5,
+    img: Company5,
+  },
+];
+
+export const pricingInfo = [
+  {
+    image: Icon1,
+    name: "Bitcoin",
+    price: "31,020.564 $",
+  },
+  {
+    image: Icon2,
+    name: "Ethereum",
+    price: "4.4668 $",
+  },
+  {
+    image: Icon3,
+    name: "Ripple",
+    price: "0.6549 $",
+  },
+];
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# cryptocurrency-project--code
-Cryptex is a fully responsive cryptocurrency website, Responsive for all devices, build using HTML, CSS, and JavaScript. cryptocurrency-website responsive- 
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
